80071beb3724e86ed182dfc14398be1b62a70d42
Yeager1711 <huynhnam171102@gmail.com>
2025-05-02 13:39:23 +0700

    template wedding invite

---
+++ data.js
@@ -0,0 +1,72 @@
+export default {
+  banner: {
+    image: "./images/2.png",
+  },
+  couple: {
+    names: "Nhị & Cường",
+    groom: {
+      name: "Nhị",
+      image: "./images/1.png",
+    },
+    bride: {
+      name: "Cường",
+      image: "./images/3.png",
+    },
+  },
+  invitation: {
+    title: "Thư Mời",
+    subtitle: "Tham Dự Lễ Cưới",
+    dayOfWeek: "Thứ 7",
+    time: "11:00",
+    day: "17",
+    month: "Tháng 11",
+    year: "2025",
+    lunarDate: "Tức Ngày 18 Tháng 09 Năm Ất Tỵ",
+    monthYear: "Tháng 11 2025",
+  },
+  loveQuote_1: "Hôn nhân là chuyện cả đời",
+  loveQuote_2: "Yêu người vừa ý, cưới người mình thương, ...",
+  familyInfo: {
+    groomFamily: {
+      title: "Nhà Trai",
+      father: "Nguyễn Văn A",
+      mother: "Trần Thị B",
+    },
+    brideFamily: {
+      title: "Nhà Gái",
+      father: "Lê Văn C",
+      mother: "Phạm Thị D",
+    },
+  },
+  coupleImages: [
+    { src: "./images/4.png", alt: "Couple 1", isCenter: false },
+    { src: "./images/5.png", alt: "Couple 2", isCenter: true },
+    { src: "./images/6.png", alt: "Couple 3", isCenter: false },
+  ],
+  eventDetails: "TIỆC MỪNG VU QUY",
+  calendar: {
+    month: "Tháng 11",
+    year: "2025",
+    days: [
+      "", "", "", "", "", "", "1",
+      "2", "3", "4", "5", "6", "7", "8",
+      "9", "10", "11", "12", "13", "14", "15",
+      "16", "17", "18", "19", "20", "21", "22",
+      "23", "24", "25", "26", "27", "28", "29",
+      "30"
+    ],
+    highlightDay: "17",
+  },
+  location: {
+    mapEmbedUrl:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4609.540003734593!2d106.64545227566936!3d10.7031259605402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175311ad90bd4bb%3A0xf92c25e13e35ed88!2sChung%20c%C6%B0%20Saigon%20Intela!5e1!3m2!1svi!2s!4v1746162553791!5m2!1svi!2s",
+  },
+
+  thumnailImages: [
+    // { src: "./images/7.png", alt: "Couple 1", isCenter: false },
+    { src: "./images/8.png", alt: "Couple 2", isCenter: true },
+    { src: "./images/9.png", alt: "Couple 3", isCenter: false },
+    { src: "./images/10.png", alt: "Couple 4", isCenter: false },
+    { src: "./images/11.png", alt: "Couple 5", isCenter: false },
+  ],
+};
