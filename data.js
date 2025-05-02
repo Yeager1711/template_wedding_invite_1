61b2f4e75b32a730c65a5c0d5912cf3d52b30652
Yeager1711 <huynhnam171102@gmail.com>
2025-05-02 21:45:14 +0700

    add file

--- data.js
+++ data.js
@@ -3,18 +3,18 @@ export default {
     image: "./images/2.png",
   },
   couple: {
-    names: "Nhị & Cường",
+    names: "Anh Tú & Diệu Nhi",
     groom: {
-      name: "Nhị",
+      name: "Anh Tú",
       image: "./images/1.png",
     },
     bride: {
-      name: "Cường",
+      name: "Diệu Nhi",
       image: "./images/3.png",
     },
   },
   invitation: {
-    title: "Thư Mời",
+    title: "Thư Mời Tiệc Cưới",
     subtitle: "Tham Dự Lễ Cưới",
     dayOfWeek: "Thứ 7",
     time: "11:00",
@@ -58,9 +58,20 @@ export default {
     highlightDay: "17",
   },
   location: {
-    mapEmbedUrl:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4609.540003734593!2d106.64545227566936!3d10.7031259605402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175311ad90bd4bb%3A0xf92c25e13e35ed88!2sChung%20c%C6%B0%20Saigon%20Intela!5e1!3m2!1svi!2s!4v1746162553791!5m2!1svi!2s",
+    groomLocation: {
+      name: "Nhà Trai - Tòa nhà Saigon Intela",
+      address: "KDC 13E, Đô thị mới Nam Thành Phố, Ấp 5, Bình Chánh, Hồ Chí Minh",
+      mapEmbedUrl:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4609.540003734593!2d106.64545227566936!3d10.7031259605402!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3175311ad90bd4bb%3A0xf92c25e13e35ed88!2sChung%20c%C6%B0%20Saigon%20Intela!5e1!3m2!1svi!2s!4v1746162553791!5m2!1svi!2s"
+    },
+    brideLocation: {
+      name: "Nhà Gái - Long Tiên, Cai Lậy, Tiền Giang",
+      address: "Long Tiên, Cai Lậy, Tiền Giang, Việt Nam",
+      mapEmbedUrl:
+        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37245.59920195017!2d106.12640493429113!3d10.350440060243987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x310a987aaf5e627d%3A0xcb50b1100ee40ede!2zTG9uZyBUacOqbiwgQ2FpIEzhuq15LCBUaeG7gW4gR2lhbmcsIFZp4buHdCBOYW0!5e1!3m2!1svi!2s!4v1746196120855!5m2!1svi!2s"
+    }
   },
+  
 
   thumnailImages: [
     // { src: "./images/7.png", alt: "Couple 1", isCenter: false },
